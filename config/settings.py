2e17e1d17a334a5a9150a013edd491758ffc49f2
niezgodak <karolinaniezgoda@gmail.com>
2022-04-09 15:27:26 +0200

    basic words model and project configuration part 2

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'words',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +78,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'selflearner',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'postgres',
+        'PASSWORD': 'coderslab',
+        'HOST': '127.0.0.1'
     }
 }
 
--- words/models.py
+++ words/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Languages(models.Model):
+    language_name = models.CharField(max_length=100)
+    word_groups = models.ForeignKey("WordGroup", on_delete=models.PROTECT)
+
+class Word(models.Model):
+    your_language = models.CharField(max_length=100)
+    foreign_language = models.CharField(max_length=100)
+    example_of_use = models.TextField(null=True)
+    is_learned = models.BooleanField(default=False)
+    #user = add user models realation -> user from session? ForeignKey
+    #language -> I don't know yet if it is needed
+
+class WordGroup(models.Model):
+    name = models.CharField(max_length=100)
+    words = models.ManyToManyField("Word")
+
+
