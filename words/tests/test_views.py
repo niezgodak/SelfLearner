6bab44ea4f98eb6b5d9a2cb9f76c101b274d2d9c
niezgodak <karolinaniezgoda@gmail.com>
2022-04-23 13:47:58 +0200

    Testing courses in progress

--- words/tests/test_views.py
+++ words/tests/test_views.py
@@ -1,44 +1,47 @@
-import pytest as pytest
-from django.contrib.auth.models import Group
-from django.test import TestCase, Client
-from django.urls import reverse, reverse_lazy
-from pytest_django.asserts import assertTemplateUsed
-from users.models import Account
-from words.models import Word, WordGroup, Languages
-import json
-
-@pytest.fixture(scope='function')
-def user(db, django_user_model):
-    user = django_user_model.objects.create_user(email='test@test.pl', name='testname', password='123')
-    return user
-
-@pytest.fixture(scope='function')
-def user2(db, django_user_model):
-    user = django_user_model.objects.create_user(email='test2@test2.pl', name='testname2', password='123')
-    return user2
-
-@pytest.fixture(scope='function')
-def group(db):
-    language = Languages.objects.create(language_name='testlanguage')
-    group = WordGroup.objects.create(name='testgroup', language=language)
-    return group
-
-@pytest.fixture(scope='function')
-def word(db):
-    Group.objects.create(name='teachers')
-    word = Word.objects.create(your_language='Mleko', foreign_language='Milk',
-                               example_of_use='Test exmaple', is_learned=False,
-                               counter=0)
-    return word
-
-
+# import pytest as pytest
+# from django.contrib.auth.models import Group
+# from django.test import TestCase, Client
+# from django.urls import reverse, reverse_lazy
+# from pytest_django.asserts import assertTemplateUsed
+# from users.models import Account
+# from words.models import Word, WordGroup, Languages
+# import json
+#
+# @pytest.fixture(scope='function')
+# def user(db, django_user_model):
+#     user = django_user_model.objects.create_user(email='test@test.pl', name='testname', password='123')
+#     return user
+#
+# @pytest.fixture(scope='function')
+# def user2(db, django_user_model):
+#     user = django_user_model.objects.create_user(email='test2@test2.pl', name='testname2', password='123')
+#     return user2
+#
+# @pytest.fixture(scope='function')
+# def group(db):
+#     language = Languages.objects.create(language_name='testlanguage')
+#     group = WordGroup.objects.create(name='testgroup', language=language)
+#     return group
+#
+# @pytest.fixture(scope='function')
+# def word(db):
+#     Group.objects.create(name='teachers')
+#     word = Word.objects.create(your_language='Mleko', foreign_language='Milk',
+#                                example_of_use='Test exmaple', is_learned=False,
+#                                counter=0)
+#     return word
+#
+#
 # def test_languages_view_get(user):
+#     """Testing view's status code"""
 #     client = Client()
 #     client.force_login(user=user)
 #     response = client.get(reverse('words:languages'))
 #     assert response.status_code == 200
 #
+#
 # def test_wordgroups_view_get(user, group):
+#     """Testting view's statuds code"""
 #     client = Client()
 #     client.force_login(user=user)
 #     num = group.language.pk
@@ -46,6 +49,7 @@ def word(db):
 #     assert response.status_code == 200
 #
 # def test_addwordgroups_view_post(user):
+#     """Testing adding new group"""
 #     client = Client()
 #     client.force_login(user=user)
 #     language = Languages.objects.create(language_name='testlanguage2')
@@ -57,6 +61,7 @@ def word(db):
 #     assert len(groups) == 1
 #
 # def test_addwordgroups_view_post_nodata(user):
+#     """Testing adding new group with invalid data"""
 #     client = Client()
 #     client.force_login(user=user)
 #     language = Languages.objects.create(language_name='testlanguage2')
@@ -66,6 +71,7 @@ def word(db):
 #     assert len(groups) == 0
 #
 # def test_addwordgroups_view_post_incorrectdata(user):
+#     """Testing adding new group with invalid data"""
 #     client = Client()
 #     client.force_login(user=user)
 #     language = Languages.objects.create(language_name='testlanguage2')
@@ -76,6 +82,8 @@ def word(db):
 #
 #
 # def test_delete_wordgroup_view_post(group, user):
+#     """Testing deleting group"""
+#
 #     client = Client()
 #     client.force_login(user=user)
 #     language = Languages.objects.create(language_name='testlanguage2')
@@ -84,6 +92,7 @@ def word(db):
 #     assert len(groups) == 0
 #
 # def test_delete_word_view(group, user, word):
+#     """Testing deleting word"""
 #     client = Client()
 #     client.force_login(user=user)
 #     group.words.add(word)
@@ -94,7 +103,10 @@ def word(db):
 #                                                                'word_pk': word.id}))
 #     words = group.words.all()
 #     assert len(words) == 0
+#
 # def test_word_create_get(user, group):
+#     """Testing WordCreateView's template used"""
+#
 #     client = Client()
 #     client.force_login(user=user)
 #     group.user.add(user)
@@ -104,6 +116,7 @@ def word(db):
 #     assertTemplateUsed(response, 'words/word_form.html')
 #
 # def test_word_create_post(user, group):
+#     """Testing WordCreateView's - creating new word"""
 #     client = Client()
 #     client.force_login(user=user)
 #     group.user.add(user)
@@ -115,6 +128,8 @@ def word(db):
 #     assert len(words) == 1
 #
 # def test_word_create_post_nodata(user, group):
+#     """Testing WordCreateView's - creating new word with no data"""
+#
 #     client = Client()
 #     client.force_login(user=user)
 #     group.user.add(user)
@@ -125,6 +140,7 @@ def word(db):
 #     assert len(words) == 0
 #
 # def test_word_create_post_incorrectdata(user, group):
+#     """Testing WordCreateView's - creating new word with incomplete data"""
 #     client = Client()
 #     client.force_login(user=user)
 #     group.user.add(user)
@@ -136,6 +152,7 @@ def word(db):
 #
 #
 # def test_learning_view(user, group):
+#     """Testing LearningView's template used and response status code"""
 #     client = Client()
 #     client.force_login(user=user)
 #     group.user.add(user)
@@ -146,6 +163,7 @@ def word(db):
 #
 #
 # def test_share_group_share(user, group):
+#     """Testing SharingView's template used and response status code"""
 #     client = Client()
 #     client.force_login(user=user)
 #     group.user.add(user)
@@ -153,17 +171,10 @@ def word(db):
 #                                                               'user_pk': user.id}))
 #     assert response.status_code == 200
 #     assertTemplateUsed(response, 'words/wordgroup_share.html')
-
+#
 # def test_word_api_get(word):
+#     """Testing WordAPIView's template used and response status code"""
 #     client = Client()
 #     response = client.get(reverse('words:wordsdataupdate', kwargs={'pk': word.id}))
 #     assert response.status_code == 200
-
-# def test_word_api_put(word):
-#     client = Client()
-#     data = {
-#         'counter': 3,
-#         'is_learned': True
-#     }
-#     response = client.put(reverse('words:wordsdataupdate', kwargs={'pk': word.id}), {'counter': 3, 'is_learned': True})
-#     assert word.counter == 3
+#
