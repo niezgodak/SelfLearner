137569a5b0264c30046a320f409717d3848eafef
niezgodak <karolinaniezgoda@gmail.com>
2022-04-24 12:23:19 +0200

    24.04.2022

--- words/tests/test_models.py
+++
@@ -1,7 +0,0 @@
-import pytest as pytest
-from django.contrib.auth.models import Group
-from django.test import TestCase, Client
-from django.urls import reverse, reverse_lazy
-from words.models import Word, WordGroup, Languages
-import json
-
